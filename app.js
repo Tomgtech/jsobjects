09edf27527e32ea2b2eaff347dc50f7cdf583589
Tomgtech <tom.gendall@yahoo.com>
2024-01-16 10:50:00 +0000

    owh yeah

---
+++ app.js
@@ -0,0 +1,25 @@
+console.log("Hello World");
+
+const person = {
+  name: "bob",
+  age: 30,
+  favouriteColour: "red",
+};
+console.log(person);
+console.table(person);
+
+const car = {
+  make: "VW",
+  model: "UP!",
+  colour: "blue",
+};
+console.log(car);
+console.table(car);
+
+const book = {
+  title: "The Dark Tower",
+  author: "Stephen King",
+  pages: "224",
+};
+console.log(book);
+console.table(book);
